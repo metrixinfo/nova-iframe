882d1a7d3ff9308b3e9aa993a82b03bd20288d9c
Michael J Love <mjlovefl@gmail.com>
2022-05-13 21:47:12 -0400

    Fix for Nova v4.

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,10 +1,19 @@
 <template>
-    <PanelItem :field="field">
-        <template slot="value">
-            <iframe v-if="showIframe" :width="width" :height="height" :class="classes" :style="style" :srcdoc="field.value"></iframe>
-            <p v-else>—</p>
-        </template>
-    </PanelItem>
+    <div class="flex flex-col md:flex-row -mx-6 px-6 py-2 md:py-0 space-y-2 md:space-y-0 border-t border-gray-100 dark:border-gray-700">
+
+        <div class="md:w-1/4 md:py-3">
+            <h4 class="font-bold md:font-normal">
+                <span>{{ field.name }}</span>
+            </h4>
+        </div>
+        <div class="md:w-3/4 md:py-3">
+            <div>
+                <iframe v-if="showIframe" :width="width" :height="height" :class="classes" :style="style" :srcdoc="field.value"></iframe>
+                <p v-else>—</p>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
